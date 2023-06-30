892470ad68f78a09595bfde6c85fd35371ee01c5
Vũ Thiên Ân <anvu1911@gmail.com>
2023-06-30 16:59:04 +0700

    updated line segmentation

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,77 +1,77 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import streamlit as st
 from PIL import Image
 
 
-def model(text_picture):
-    img = np.array(text_picture.convert("RGB"))
+def thresholding(img):
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(img_gray, 120, 255, cv2.THRESH_BINARY_INV)
+    return thresh
+
+
+def resizing(img):
+    img = np.asarray(img)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     h, w, c = img.shape
     if w > 1000:
         new_w = 1000
-        ar = w/h
+        ar = w / h
         new_h = int(new_w/ar)
-        img = cv2.resize(img, (new_w, new_h), interpolation = cv2.INTER_AREA)
+        img = cv2.resize(img, (new_w, new_h), interpolation=cv2.INTER_AREA)
+    return img
 
-    #plt.imshow(img)
-    def thresholding(image):
-        img_gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        ret,thresh = cv2.threshold(img_gray,80,255,cv2.THRESH_BINARY_INV)
-        plt.imshow(thresh, cmap='gray')
-        return thresh
+
+def dilating(img):
+    kernel = np.ones((5, 100), np.uint8)
+    dilated = cv2.dilate(img, kernel, iterations=1)
+    return dilated
+
+
+def segmenting(img, dilated):
+    (contours, hierarchy) = cv2.findContours(
+        dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    sorted_contours_lines = sorted(
+        contours, key=lambda ctr: cv2.boundingRect(ctr)[1])  # (x, y of top left corner, w, h)
+
+    img2 = img.copy()
+
+    for ctr in sorted_contours_lines:
+        x, y, w, h = cv2.boundingRect(ctr)
+
+        x1 = x
+        y1 = y
+        x2 = x + w
+        y2 = y + h
+        cropped_image = img2[y1:y2, x1:x2]
+        st.image(cropped_image, use_column_width=True)
+
+
+def segmentation(text_picture):
+    img = resizing(text_picture)
 
     thresh_img = thresholding(img)
-    #dilation
-    kernel = np.ones((3,85), np.uint8)
-    dilated = cv2.dilate(thresh_img, kernel, iterations = 1)
-    plt.imshow(dilated, cmap='gray')
-    (contours, heirarchy) = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    sorted_contours_lines = sorted(contours, key = lambda ctr : cv2.boundingRect(ctr)[1]) # (x, y, w, h)
-    
-    img2 = img.copy()
-    for ctr in sorted_contours_lines:
-        x,y,w,h = cv2.boundingRect(ctr)
-        cv2.rectangle(img2, (x,y), (x+w, y+h), (40, 100, 250), 2)
-    
-    #dilation
-    kernel = np.ones((3,15), np.uint8)
-    dilated2 = cv2.dilate(thresh_img, kernel, iterations = 1)
-    plt.imshow(dilated2, cmap='gray')
 
-    img3 = img.copy()
-    words_list = []
-    for line in sorted_contours_lines:
-        # roi of each line
-        x, y, w, h = cv2.boundingRect(line)
-        roi_line = dilated2[y:y+w, x:x+w]
-        # draw contours on each word
-        (cnt, heirarchy) = cv2.findContours(roi_line.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        sorted_contour_words = sorted(cnt, key=lambda cntr : cv2.boundingRect(cntr)[0])
+    dilated_img = dilating(thresh_img)
 
-        for word in sorted_contour_words:
-            if cv2.contourArea(word) < 400:
-                continue
-            x2, y2, w2, h2 = cv2.boundingRect(word)
-            words_list.append([x+x2, y+y2, x+x2+w2, y+y2+h2])
-            cv2.rectangle(img3, (x+x2, y+y2), (x+x2+w2, y+y2+h2), (255,255,100),2)
+    st.header("Dilated lines")
+    st.image(dilated_img, use_column_width=True)
 
-    ninth_word = words_list[8]
-    roi_9 = img[ninth_word[1]:ninth_word[3], ninth_word[0]:ninth_word[2]]
-    return roi_9
+    st.header("Segmented regions")
+    segmenting(img, dilated_img)
 
 
 def main():
     st.title("Handwritting Recognition")
-    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    uploaded_file = st.file_uploader(
+        "Choose an image...", type=["jpg", "jpeg", "png"])
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
         st.image(image, use_column_width=True)
-    if st.button("Scan"):  # check if the "Predict" button has been clicked
-        value = model(image)
-        st.write("Result:")
-        st.image(value, use_column_width=True)
+        # check if the "Predict" button has been clicked
+        if image is not None and st.button("Scan"):
+            segmentation(image)
+
 
 if __name__ == "__main__":
     main()
